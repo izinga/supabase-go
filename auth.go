265a76319604fd19ff0524ae54f6dac0e3189ef7
Om Narayan <omnarayan@robustest.com>
2024-09-09 03:13:55 +0530

    Update auth.go

--- auth.go
+++ auth.go
@@ -279,6 +279,8 @@ func (a *Auth) UpdateUser(ctx context.Context, userToken string, updateData map[
 func (a *Auth) ResetPasswordForEmail(ctx context.Context, email string, redirectURL string) error {
 	reqBody, _ := json.Marshal(map[string]string{"email": email})
 	reqURL := fmt.Sprintf("%s/%s/recover?redirect_to=%s", a.client.BaseURL, AuthEndpoint, redirectURL)
+	
+	fmt.Println("ResetPasswordForEmail", reqURL)
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, reqURL, bytes.NewBuffer(reqBody))
 	if err != nil {
 		return err
